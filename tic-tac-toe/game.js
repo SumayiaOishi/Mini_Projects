768e72f4d2a12ba035d6d7ed928f364ef897e3e4
SumayiaOishi <sumayia.alam04@gmail.com>
2024-03-31 21:28:59 +0600

    winner, resetbutton and new_game_button is created

--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -1,5 +1,8 @@
 let boxes =document.querySelectorAll(".box");
 let reset=document.querySelector("#reset");
+let msg =document.querySelector("#msg");
+let container=document.querySelector(".msg-container");
+let newGame=document.querySelector("#new-game");
 
 let turn0=true;
 const winPatterns=[
@@ -12,6 +15,11 @@ const winPatterns=[
     [3,4,5],
     [6,7,8]
 ];
+const resetGame =()=>{
+    let turn0=true;
+    enable();
+    container.classList.add("hide");
+}
 boxes.forEach((box)=>{
     box.addEventListener("click", ()=>{
       if(turn0){
@@ -28,9 +36,36 @@ boxes.forEach((box)=>{
       checkWinner();
     });
 });
+const enable=()=>{
+    for (let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+      }
+}
+
+const showWinner=(winner)=>{
+  msg.innerText=`Congratulations! Winner is ${winner}`;
+  container.classList.remove("hide");
+  for (let box of boxes){
+    box.disabled=true;
+  }
+ 
+}
 
 const checkWinner=()=>{
     for(let pattern of winPatterns){
-        console.log(pattern);
+     let val1=boxes[pattern[0]].innerText;
+     let val2=boxes[pattern[1]].innerText;
+     let val3=boxes[pattern[2]].innerText;
+    
+   if(val1!=""&& val2!=""&& val3!=""){
+        if(val1===val2 && val2===val3){
+
+          showWinner(val1);
+        }
+   }
+
     }
 }
+newGame.addEventListener("click", resetGame);
+reset.addEventListener("click", resetGame);
